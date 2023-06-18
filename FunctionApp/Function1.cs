78f5214512a9d5a30d90fc8082cc23de80cf9d07
thaoduong-storai <thao.d@stor.ai>
2023-06-18 20:55:37 +0700

    edit format mess

--- FunctionApp/Function1.cs
+++ FunctionApp/Function1.cs
@@ -45,8 +45,9 @@ namespace FunctionApp_Example
                     var commitInfo = await githubClient.User.Get(latestCommit.Author.Login);
 
                     StringBuilder teamsMessageBuilder = new StringBuilder();
-                    teamsMessageBuilder.AppendLine("**Người commit: " + commitInfo.Name + " (" + commitInfo.Login + ")**");
-                    teamsMessageBuilder.AppendLine("**\nNội dung commit: " + latestCommit.Commit.Message + "**");
+                    teamsMessageBuilder.AppendLine("***Người commit: ***" + commitInfo.Name + commitInfo.Login );
+                    teamsMessageBuilder.AppendLine(); // Xuống dòng sau tin nhắn
+                    teamsMessageBuilder.AppendLine("***Nội dung commit: ***" + latestCommit.Commit.Message);
 
                     string teamsMessage = teamsMessageBuilder.ToString();
 
