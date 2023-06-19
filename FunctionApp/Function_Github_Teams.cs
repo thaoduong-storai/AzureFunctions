d8d20fce911d1475465f0b5df9bbf3dcff883a29
thaoduong-storai <thao.d@stor.ai>
2023-06-19 17:50:44 +0700

    changes tokennn

--- FunctionApp/Function_Github_Teams.cs
+++ FunctionApp/Function_Github_Teams.cs
@@ -31,7 +31,7 @@ namespace FunctionApp_Example
                 return new BadRequestObjectResult("Vui lòng cung cấp thông tin chính xác về repository.");
             }
 
-            string githubAccessToken = "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";
+            string githubAccessToken = "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";
             var githubClient = new GitHubClient(new ProductHeaderValue("Azure-Function-GitHub"));
             githubClient.Credentials = new Credentials(githubAccessToken);
 
